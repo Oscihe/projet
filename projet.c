019644b2b8f8f207a2a1ebcd5e7b8ead74f622dc
Oscihe <92468626+Oscihe@users.noreply.github.com>
2021-10-13 20:47:13 +0200

    Add files via upload

---
+++ projet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+struct précipitation {
+	double x;
+	double y;
+	float altitude;
+	double précipitation_totale_surface_choisie
+echo "# projet" >> README.md
+git init;
+git add README.md;
+git commit -m "first commit";
+git branch -M main;
+git remote add origin https://github.com/Oscihe/projet.git;
+git push -u origin main;
