a9e03953321e0ee50292b9fee8b8a3f10164d967
Oscar Hellmüller <oscar.hellmuller@epfl.ch>
2021-11-17 18:58:47 +0100

    Controle

--- dingdonc.c
+++ dingdonc.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <time.h>
-struct Gpspoint{
-  double q;
-  double w;
-  float e;
-};
-struct Gpspoint talala[37*44];
+#include <math.h>
 
-int lireligne(char*ligne,struct Gpspoint*point){
+
+// Structure
+struct GpsPoint {
+    double latitude;
+    double longitude;
+    float altitude;
+};
+
+// Lire une ligne et remplir la structure
+int lireLigne(char * ligne, struct GpsPoint * point) {
     char * virgule1 = strchr(ligne, ' ');
     if (virgule1 == NULL) return 0;
     char * virgule2 = strchr(virgule1 + 1, ' ');
     if (virgule2 == NULL) return 0;
-    point->q = atof(ligne);
-    point->w= atof(virgule1 + 1);
-    point->e = atof(virgule2 + 1);
+    double v1=atof(ligne);
+    double v2=atof(virgule1+1);
+    double v3=atof(virgule2+1);
+    //printf("%f,%f,%f\n", v1,v2,v3);
+    if(v1 >= 592800.00 && v1 <= 600000.00 && v2 >= 095200.00 && v2 <= 103800.00){
+        //Le compilateur ne comprend pas les deux conditions "<="
+        //printf("%s\n","ok" );
+        point->latitude = v1;
+        point->longitude = v2;
+        point->altitude = v3;
+        printf("%f,%f,%f\n", point->longitude,point->latitude,point->altitude);
+        //printf("%f\n",point->altitude );
+        //printf("%f\n",point->alti );
+    }
     return 1;
-    return 0;
 }
+
+int lireFichier(char * nomFichier, struct GpsPoint * tableauARemplir, int longueur) {
+    // Ouvrir le fichier
+    FILE * file = fopen(nomFichier, "r");
+    if (file == NULL) return -1;
+
+    // Lire ligne par ligne
+    int n = 0;
+    char buffer[100];
+    while (fgets(buffer, 100, file) != NULL) {
+        if (n >= longueur) break;
+        int ok = lireLigne(buffer, &tableauARemplir[n]);
+        if (ok) n = n + 1;
+    }
+
+    // Fermer le fichier et renvoyer le nombre de lignes lues
+    fclose(file);
+    return n;
+}
+
 int main(int argc, char * argv[]) {
-  //double ymax=...;
-  //double xmin=...;
-  //double index=(ymax-y)/200*37+(x-xmin)/200;
-  FILE * fichier = fopen("DHM200.xyz", "r");
-  char ligne[100];
-//  double * altitudes = malloc(10000000 * sizeof (double));
-  int i=0;
-  while (fgets(ligne, 100, fichier) != NULL) {
-    lireligne(ligne,&);
-    //printf("%d,%d,%d",x,y,z);
-    //if(592800.00<=x<=600000.00&&095200.00<=y<=103800.00){
-    //  altitudes[index]=z;
-    //}
-    i++;
+    // Lire le fichier
+    struct GpsPoint * points = malloc(15000000*sizeof(struct GpsPoint));
+    int nbPoints = lireFichier("DHM200.xyz", points, 15000000*sizeof(double));
+    if (nbPoints == -1) {
+        printf("Erreur: Le fichier n'existe pas ou n'est pas accessible.\n");
+        return 1;
+    } else if (nbPoints == 0) {
+        printf("Erreur: Le fichier est vide ou pas dans le bon format.\n");
+        return 1;
+    }
+    printf("%f\n", points);
 
-    //altitudes[i]=atof(ok);
-    //double valeur = atof(ligne);
-  //  printf("%s",ligne);
-
-
-
-
-  }
-fclose(fichier);
+    return 0;
 }
